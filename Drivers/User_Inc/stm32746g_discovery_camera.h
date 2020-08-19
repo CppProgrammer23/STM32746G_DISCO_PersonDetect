10079d3149c1fd6f2d9d5b3edaf8de8632c5793f
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-08-19 16:41:20 +0100

    Add files via upload

---
+++ Drivers/User_Inc/stm32746g_discovery_camera.h
@@ -0,0 +1,217 @@
+/**
+  ******************************************************************************
+  * @file    stm32746g_discovery_camera.h
+  * @author  MCD Application Team
+  * @brief   This file contains the common defines and functions prototypes for
+  *          the stm32746g_discovery_camera.c driver.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */ 
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32746G_DISCOVERY_CAMERA_H
+#define __STM32746G_DISCOVERY_CAMERA_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+/* Includes ------------------------------------------------------------------*/
+/* Include Camera component Driver */
+//#include "../Components/ov9655/ov9655.h"
+#include "ov9655.h"
+#include "stm32746g_discovery.h"
+
+/** @addtogroup BSP
+  * @{
+  */
+
+/** @addtogroup STM32746G_DISCOVERY
+  * @{
+  */
+    
+/** @addtogroup STM32746G_DISCOVERY_CAMERA
+  * @{
+  */ 
+#define CAMERA_INSTANCES_NBR           1U
+
+#define CAMERA_MODE_CONTINUOUS         DCMI_MODE_CONTINUOUS
+#define CAMERA_MODE_SNAPSHOT           DCMI_MODE_SNAPSHOT
+
+/* Camera resolutions */
+#define CAMERA_R160x120                 0U     /* QQVGA Resolution            */
+#define CAMERA_R320x240                 1U     /* QVGA Resolution             */
+#define CAMERA_R480x272                 2U     /* 480x272 Resolution          */
+#define CAMERA_R640x480                 3U     /* VGA Resolution              */
+#define CAMERA_R800x480                 4U     /* WVGA Resolution             */
+
+/* Camera Pixel Format */
+#define CAMERA_PF_RGB565                0U     /* Pixel Format RGB565         */
+#define CAMERA_PF_RGB888                1U     /* Pixel Format RGB888         */
+#define CAMERA_PF_YUV422                2U     /* Pixel Format YUV422         */
+
+/* Brightness */
+#define CAMERA_BRIGHTNESS_MIN          -4
+#define CAMERA_BRIGHTNESS_MAX           4
+
+/* Saturation */
+#define CAMERA_SATURATION_MIN          -4
+#define CAMERA_SATURATION_MAX           4
+
+/* Contrast */
+#define CAMERA_CONTRAST_MIN            -4
+#define CAMERA_CONTRAST_MAX             4
+
+/* Hue Control */
+#define CAMERA_HUEDEGREE_MIN           -6
+#define CAMERA_HUEDEGREE_MAX            5
+
+/* Mirror/Flip */
+#define CAMERA_MIRRORFLIP_NONE          0x00U   /* Set camera normal mode     */
+#define CAMERA_MIRRORFLIP_FLIP          0x01U   /* Set camera flip config     */
+#define CAMERA_MIRRORFLIP_MIRROR        0x02U   /* Set camera mirror config   */
+
+/* Zoom */
+#define CAMERA_ZOOM_x8                  0x00U   /* Set zoom to x8             */
+#define CAMERA_ZOOM_x4                  0x11U   /* Set zoom to x4             */
+#define CAMERA_ZOOM_x2                  0x22U   /* Set zoom to x2             */
+#define CAMERA_ZOOM_x1                  0x44U   /* Set zoom to x1             */
+
+/* Color Effect */
+#define CAMERA_COLOR_EFFECT_NONE        0x00U   /* No effect                  */
+#define CAMERA_COLOR_EFFECT_BLUE        0x01U   /* Blue effect                */
+#define CAMERA_COLOR_EFFECT_RED         0x02U   /* Red effect                 */
+#define CAMERA_COLOR_EFFECT_GREEN       0x04U   /* Green effect               */
+#define CAMERA_COLOR_EFFECT_BW          0x08U   /* Black and White effect     */
+#define CAMERA_COLOR_EFFECT_SEPIA       0x10U   /* Sepia effect               */
+#define CAMERA_COLOR_EFFECT_NEGATIVE    0x20U   /* Negative effect            */
+
+/* Light Mode */
+#define CAMERA_LIGHT_AUTO               0x00U   /* Light Mode Auto            */
+#define CAMERA_LIGHT_SUNNY              0x01U   /* Light Mode Sunny           */
+#define CAMERA_LIGHT_OFFICE             0x02U   /* Light Mode Office          */
+#define CAMERA_LIGHT_HOME               0x04U   /* Light Mode Home            */
+#define CAMERA_LIGHT_CLOUDY             0x08U   /* Light Mode Claudy          */
+
+/* Night Mode */
+#define CAMERA_NIGHT_MODE_SET           0x00U   /* Disable night mode         */
+#define CAMERA_NIGHT_MODE_RESET         0x01U   /* Enable night mode          */
+
+#define CAMERA_IRQHandler               DCMI_IRQHandler
+#define CAMERA_DMA_IRQHandler           DMA2_Stream3_IRQHandler
+
+
+#define CAMERA_OV9655_ADDRESS           0x60U
+   
+/** @defgroup STM32746G_DISCOVERY_CAMERA_Exported_Types STM32746G_DISCOVERY_CAMERA Exported Types
+  * @{
+  */
+  
+/** 
+  * @brief  Camera State structures definition  
+  */  
+typedef enum 
+{
+  CAMERA_OK            = 0x00,
+  CAMERA_ERROR         = 0x01,
+  CAMERA_TIMEOUT       = 0x02,
+  CAMERA_NOT_DETECTED  = 0x03,
+  CAMERA_NOT_SUPPORTED = 0x04
+
+} Camera_StatusTypeDef;
+
+#define RESOLUTION_R160x120      CAMERA_R160x120      /* QQVGA Resolution     */
+#define RESOLUTION_R320x240      CAMERA_R320x240      /* QVGA Resolution      */
+#define RESOLUTION_R480x272      CAMERA_R480x272      /* 480x272 Resolution   */
+#define RESOLUTION_R640x480      CAMERA_R640x480      /* VGA Resolution       */  
+/**
+  * @}
+  */ 
+ 
+/** @defgroup STM32746G_DISCOVERY_CAMERA_Exported_Constants STM32746G_DISCOVERY_CAMERA Exported Constants
+  * @{
+  */
+#define BSP_CAMERA_IRQHandler      DCMI_IRQHandler
+#define BSP_CAMERA_DMA_IRQHandler  DMA2_Stream1_IRQHandler  
+
+/**
+  * @}
+  */
+
+/** @addtogroup STM32746G_DISCOVERY_CAMERA_Exported_Functions
+  * @{
+  */    
+uint8_t BSP_CAMERA_Init(uint32_t Resolution);  
+uint8_t BSP_CAMERA_DeInit(void);
+void    BSP_CAMERA_ContinuousStart(uint8_t *buff);
+void    BSP_CAMERA_SnapshotStart(uint8_t *buff);
+void    BSP_CAMERA_Suspend(void);
+void    BSP_CAMERA_Resume(void);
+uint8_t BSP_CAMERA_Stop(void); 
+void    BSP_CAMERA_PwrUp(void);
+void    BSP_CAMERA_PwrDown(void);
+void    BSP_CAMERA_LineEventCallback(void);
+void    BSP_CAMERA_VsyncEventCallback(void);
+void    BSP_CAMERA_FrameEventCallback(void);
+void    BSP_CAMERA_ErrorCallback(void);
+
+/* Camera features functions prototype */
+void    BSP_CAMERA_ContrastBrightnessConfig(uint32_t contrast_level, uint32_t brightness_level);
+void    BSP_CAMERA_BlackWhiteConfig(uint32_t Mode);
+void    BSP_CAMERA_ColorEffectConfig(uint32_t Effect);
+   
+/* These functions can be modified in case the current settings (e.g. DMA stream)
+   need to be changed for specific application needs */
+void BSP_CAMERA_MspInit(DCMI_HandleTypeDef *hdcmi, void *Params);
+void BSP_CAMERA_MspDeInit(DCMI_HandleTypeDef *hdcmi, void *Params);
+
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32746G_DISCOVERY_CAMERA_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
